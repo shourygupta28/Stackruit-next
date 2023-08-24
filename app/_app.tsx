b0669af56f473768e7ea98cc5bcac86cd56b36ea
Shourya Gupta <samarth.g2001@gmail.com>
2023-08-24 14:12:20 +0530

    third

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,20 +1,20 @@
-// pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document'
+// // pages/_document.js
+// import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-                <div id="portal"></div>
-            </body>
-        </Html>
-    )
-}
+// export default function Document() {
+//     return (
+//         <Html>
+//             <Head>
+//                 <link rel="preconnect" href="https://fonts.googleapis.com" />
+//                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+//                 <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
+//                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+//             </Head>
+//             <body>
+//                 <Main />
+//                 <NextScript />
+//                 <div id="portal"></div>
+//             </body>
+//         </Html>
+//     )
+// }
